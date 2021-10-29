61e4e72b3deb7baf7e7dfd414f56ce4daf35599a
Vadimzzzzz <92207928+Vadimzzzzz@users.noreply.github.com>
2021-10-29 23:49:02 +0400

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,147 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+Widget _generateItem(int index) {
+  return new Container(
+    padding: const EdgeInsets.all(8.0),
+    child: new Stack(
+      children: <Widget>[
+        Positioned(
+            top: 95,
+            left: 170,
+            child: Container(
+                width: 200,
+                height: 200,
+                decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    gradient: RadialGradient(
+                      colors: [
+                        Color(0x9EDE5600),
+                        Color(0xFF000000),
+                      ],
+                    )))),
+        Positioned(
+            bottom: 80,
+            right: -30,
+            child: Container(
+                width: 200,
+                height: 200,
+                decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    gradient: RadialGradient(
+                      colors: [
+                        Color(0x9EDE5600),
+                        Color(0xFF000000),
+                      ],
+                    )))),
+        Positioned(
+            bottom: 150,
+            right: 5,
+            child: Container(
+                width: 200,
+                height: 460,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(70.0),
+                  color: Colors.white,
+                ),
+                child: Container(
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(70.0),
+                      child: new Image(
+                        image: AssetImage('assets/images/rick.jpg'),
+                        width: 228.0,
+                        height: 300.0,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    width: 190,
+                    height: 450,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                    )))),
+        Positioned(
+            top: 90,
+            left: -10,
+            child: Container(
+                child: new Column(children: <Widget>[
+                  Text(
+                    '28',
+                    style: TextStyle(
+                      fontFamily: '',
+                      // а у текста появился виджет, который его стилизует
+                      color: Color(0xFFFFFFFF),
+                      // задаем ему цвет текста
+                      fontSize: 100.0, // и размер шрифта
+                    ),
+                  ),
+                  Text(
+                    'окт 21:00',
+                    style: TextStyle(
+                      fontFamily: '',
+                      // а у текста появился виджет, который его стилизует
+                      color: Color(0xFFFFFFFF),
+                      // задаем ему цвет текста
+                      fontSize: 20.0, // и размер шрифта
+                    ),
+                  ),
+                ]),
+                width: 170,
+                height: 170,
+                decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    gradient: LinearGradient(
+                      colors: [
+                        Color(0xFF4C4747),
+                        Color(0xFF232328),
+                      ],
+                    )))),
+        Positioned(
+            bottom: 50,
+            left: 10,
+            child: Container(
+              child: new Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      'участвует',
+                      style: TextStyle(
+                        fontFamily: '',
+                        // а у текста появился виджет, который его стилизует
+                        color: Color(0xFFB39384),
+                        // задаем ему цвет текста
+                        fontSize: 15.0, // и размер шрифта
+                      ),
+                    ),
+                    Text(
+                      'Рик Эстли',
+                      style: TextStyle(
+                        fontFamily: '',
+                        // а у текста появился виджет, который его стилизует
+                        color: Color(0xFFC98F7C),
+                        // задаем ему цвет текста
+                        fontSize: 20.0, // и размер шрифта
+                      ),
+                    ),
+                    Text(
+                      'КОНЦЕРТ',
+                      style: TextStyle(
+                        fontFamily: '',
+                        // а у текста появился виджет, который его стилизует
+                        color: Color(0xFFE24A19),
+                        // задаем ему цвет текста
+                        fontSize: 40.0, // и размер шрифта
+                      ),
+                    ),
+                  ]),
+              width: 200,
+              height: 120,
+            )),
+      ],
+    ),
+  );
+}
+
+main() => runApp(
+      Directionality(textDirection: TextDirection.ltr, child: _generateItem(0)),
+    );
